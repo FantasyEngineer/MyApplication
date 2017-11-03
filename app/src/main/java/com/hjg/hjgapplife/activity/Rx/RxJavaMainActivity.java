22b5364f4f195b6322aefbfdb5ce2274d9cd64ac
houjiguo <347622814@qq.com>
2017-11-03 20:14:05 +0800

    rxjava

--- app/src/main/java/com/hjg/hjgapplife/activity/Rx/RxJavaMainActivity.java
+++ app/src/main/java/com/hjg/hjgapplife/activity/Rx/RxJavaMainActivity.java
@@ -1,21 +1,19 @@
 package com.hjg.hjgapplife.activity.Rx;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.hjg.baseapp.util.ToastUtil;
+import com.hjg.baseapp.widget.dialog.BottomDialog;
 import com.hjg.hjgapplife.R;
 import com.hjg.hjgapplife.activity.baseRender.BaseOthreRenderSwipFinalActivity;
 import com.hjg.hjgapplife.activity.baseRender.LayoutConstans;
-
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
@@ -23,6 +21,10 @@ public class RxJavaMainActivity extends BaseOthreRenderSwipFinalActivity {
 
     @BindView(R.id.tv_test)
     TextView tvTest;
+    @BindView(R.id.tv_emitter)
+    TextView tvEmitter;
+    @BindView(R.id.tv_disposable)
+    TextView tvDisposable;
     private Observable<String> sender;
     private Observer<String> receiver;
 
@@ -76,9 +78,19 @@ public class RxJavaMainActivity extends BaseOthreRenderSwipFinalActivity {
 
     }
 
-
-    @OnClick(R.id.tv_test)
-    public void onViewClicked() {
-
+    @OnClick({R.id.tv_test, R.id.tv_emitter, R.id.tv_disposable})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_test:
+                BottomDialog bottomDialog = new BottomDialog(activity);
+                bottomDialog.showDialog(getString(R.string.rx_framework));
+                break;
+            case R.id.tv_emitter:
+                BottomDialog bottomDialogemitter = new BottomDialog(activity);
+//                bottomDialogemitter
+                break;
+            case R.id.tv_disposable:
+                break;
+        }
     }
 }
